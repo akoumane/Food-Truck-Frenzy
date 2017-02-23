3908e5d54d3967d817201ab675ebec1ddb0b3a87
Brandon Pitcher <bpitcher@csub.edu>
2017-02-23 09:38:40 -0800

    substantial improvement to this program

--- brandonP.cpp
+++ brandonP.cpp
@@ -2,6 +2,26 @@
 // CMPS 3350 Lab 5
 // Created: 23 February 2017
 #include <iostream>
+#include <cstdlib>
+#include <iomanip>
+#include <cstring>
+#include <string>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <time.h>
+#include <math.h>
+#include <X11/Xlib.h>
+#include <X11/keysym.h>
+#include <GL/glx.h>
+#include "log.h"
+#include "ppm.h"
+
+extern "C" {
+#include "fonts.h"
+}
+
 using namespace std;
 
 void helloWorld()
@@ -12,5 +32,10 @@ void helloWorld()
 
 void printName()
 {
+	Rect r;
+	r.bot = 100;
+	r.left = 100;
+	r.center = 100;
+	unsigned int color = 0x00dddd00;
 	ggprint8b(&r, 16, color, "Brandon Pitcher!!!");
 }
