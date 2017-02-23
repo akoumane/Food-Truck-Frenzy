15c7e337035cd17be2b6d7f052d70d8988b7c1b7
akoumane <git@github.comclear>
2017-02-23 09:27:49 -0800

    Added circle function to andyK.cpp

--- andyK.cpp
+++ andyK.cpp
@@ -1,2 +1,38 @@
 #include <iostream>
 using namespace std;
+
+void drawCircle(GLfloat x, GLfloat y, GLfloat z, GLfloat radius,
+    GLint numSides)
+{
+	GLint numVertices = numSides + 2;
+    GLfloat doublePi = 2.0f * M_PI;
+
+    GLfloat circleVerticesX[numVertices];
+    GLfloat circleVerticesY[numVertices];
+    GLfloat circleVerticesZ[numVertices];
+
+    circleVerticesX[0] = x;
+    circleVerticesY[0] = y;
+    circleVerticesZ[0] = z;
+    
+    for(int i = 1; i < numVertices; i++)
+    {   
+        circleVerticesX[i] = x + (radius * cos(i * doublePi / numSides));      
+        circleVerticesY[i] = y + (radius * sin(i * doublePi / numSides));      
+        circleVerticesZ[i] = z;
+    }
+
+    GLfloat allCircleVertices[numVertices * 3];
+
+    for(int i = 0; i < numVertices; i++)
+    {
+        allCircleVertices[i * 3] = circleVerticesX[i];
+        allCircleVertices[(i * 3) + 1] = circleVerticesY[i];
+        allCircleVertices[(i * 3) + 2] = circleVerticesZ[i];
+    }
+
+    glEnableClientState(GL_VERTEX_ARRAY);
+    glVertexPointer(3, GL_FLOAT, 0, allCircleVertices);
+    glDrawArrays(GL_TRIANGLE_FAN, 0, numVertices);
+    glDisableClientState(GL_VERTEX_ARRAY);
+}
