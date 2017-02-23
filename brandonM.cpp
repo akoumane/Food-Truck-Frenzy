bf5f6c1279fa36af3144321f8bd0295e5745a7fa
bforbrandon <bmartinez40@csub.edu>
2017-02-23 09:44:59 -0800

    added my name

--- brandonM.cpp
+++ brandonM.cpp
@@ -17,8 +17,35 @@
 //look for the 
 //
 //
+#include <iostream>
+#include <cstdlib>
+#include <iomanip>
+#include <cstring>
+#include <string>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <iostream>
- using namespace std;
+#include <unistd.h>
+#include <time.h>
+#include <math.h>
+#include <X11/Xlib.h>
+#include <X11/keysym.h>
+#include <GL/glx.h>
+#include "log.h"
+#include "ppm.h"
+
+extern "C" {
+#include "fonts.h"
+}
+
+using namespace std;
+
+void printBrandonName()
+{
+	Rect r;
+	r.bot = 100;
+	r.left = 140;
+	r.center = 100;
+	unsigned int color = 0x00dddd00;
+	ggprint8b(&r, 16, color, "Brandon Martinez!!!");
+}
